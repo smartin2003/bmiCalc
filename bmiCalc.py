5a68e2b46ec2998f3a3fb6d0271dc3561615b096
smartin2003 <smartin2003@outlook.com>
2025-03-18 20:27:40 -0500

    Initial bmiCalc.py

---
+++ bmiCalc.py
@@ -0,0 +1,20 @@
+def calcBMI(height, weight):
+	heightConv = height * 0.025
+	weightConv = weight * 0.45
+	bmi = weightConv / (heightConv**2)
+
+	if bmi < 18.5:
+		category = 'Underweight'
+	elif bmi <= 24.9:
+		category = 'Normal Weight'
+	elif bmi <= 29.9:
+		category = 'Overweight'
+	else:
+		category = 'Obese'
+
+	return bmi, category
+
+print('Welcome to the BMI Calculator')
+height = int(input('What is your height in inches?'))
+weight = int(input('What is your weight in pounds?'))
+calcBMI(height, weight)
